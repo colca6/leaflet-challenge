c55999173fb718f0c3d76b62500aebbce4be75ad
Carl Colburn <carlcolburn@gmail.com>
2023-10-14 17:13:14 -0700

    Built html for map, legend and title

--- Leaflet-Part1/static/js/logic.js
+++ Leaflet-Part1/static/js/logic.js
@@ -1,3 +1,7 @@
+
+  //reference the link to the endpoint for earthquake data
+  url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
 // Creating our initial map object:
 // We set the longitude, latitude, and starting zoom level.
 // This gets inserted into the div with an id of "map".
@@ -12,3 +16,38 @@ let myMap = L.map("map", {
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
+
+//Create HTML title in a layer to show on map
+    var info = L.control({position:"topright"});
+
+    info.onAdd = function() {
+        var div = L.DomUtil.create("div","info");
+        var title = "<h1>US Earthquake Depth in past 7 Days</h1>"
+        div.innerHTML = title;
+
+        return div
+    };
+
+//Create HTML legend showing EQ depth in a layer to show on map
+    var legend  = L.control({ position : "bottomright"});
+
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "legend");
+        var limits = [-10, 10, 30, 50, 70, 90];
+        //var title = "<h2>EQ Depth in km</h2>"
+        var title = ""
+
+        div.innerHTML = title;
+
+        for (var i = 0; i < limits.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + colorCircle(limits[i] + 1) + '"></i> ' +
+                limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+//Add legend and title to map
+    legend.addTo(myMap);
+    info.addTo(myMap);
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # leaflet-challenge
 Mod 15 homework
+
+
+1. Started with Initiating the map.
+For this, referred to Exercise 15.1.6, instructor demo for city population
+This shows using openstreetmap.org to get image for the map tile layer.  Also showing attribution.
+
+
+For positioning legend and title (Depth in bottom right and Title upper right), used leaflet layer control. Referred to documentation
+at https://gis.stackexchange.com/questions/41768/leaflet-how-to-move-the-layer-control-menu
